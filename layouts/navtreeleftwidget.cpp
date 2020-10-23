b6e7d3937d87a42544ea244385d580c3ab5e586a
HELI <waltz_h@163.com>
2020-10-23 16:19:46 +0800

    Update

--- layouts/navtreeleftwidget.cpp
+++ layouts/navtreeleftwidget.cpp
@@ -1,6 +1,8 @@
 #include "navtreeleftwidget.h"
 #include "ui_navtreeleftwidget.h"
 
+#include <QDebug>
+
 NavTreeLeftWidget::NavTreeLeftWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::NavTreeLeftWidget)
@@ -28,24 +30,25 @@ void NavTreeLeftWidget::initForm()
     rootItem->setText(0, "菜单0");
     rootItem->setIcon(0, icon1);
     rootItem->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
-    rootItem->setData(0, Qt::UserRole, "folder");
+    rootItem->setData(0, Qt::UserRole, "0");
 
     QTreeWidgetItem *childItem1;
     childItem1 = new QTreeWidgetItem(ui->navTreeWidget);
     childItem1->setText(0, "菜单1");
     childItem1->setIcon(0, icon1);
-    rootItem->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
-    rootItem->setData(0, Qt::UserRole, "folder");
+    childItem1->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+    childItem1->setData(0, Qt::UserRole, "1");
 
     QTreeWidgetItem *childItem2;
     childItem2 = new QTreeWidgetItem(ui->navTreeWidget);
     childItem2->setText(0, "菜单2");
     childItem2->setIcon(0, icon1);
-    rootItem->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
-    rootItem->setData(0, Qt::UserRole, "folder");
+    childItem2->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+    childItem2->setData(0, Qt::UserRole, "2");
 }
 
 void NavTreeLeftWidget::on_navTreeWidget_itemClicked(QTreeWidgetItem *item, int column)
 {
-
+    int id = item->data(0, Qt::UserRole).value<int>();
+    ui->stackedWidget->setCurrentIndex(id);
 }
